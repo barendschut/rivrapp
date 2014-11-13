5851044e0401231ecb5a1b1f25f20d0124320437
Jeroen <mail@jeroenvdijk.com>
2014-11-13 10:56:22 +0100

    Java web.xml equiv. changed --> set the context path and removed old code

--- src/main/java/nl/ing/hackathon/ServletInit.java
+++ src/main/java/nl/ing/hackathon/ServletInit.java
@@ -1,6 +1,8 @@
 package nl.ing.hackathon;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -18,7 +20,16 @@ public class ServletInit extends SpringBootServletInitializer {
 	}
 
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
 		return application.sources(ServletInit.class);
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+		factory.setPort(8080);
+		factory.setContextPath("/api2ivr");
+		return factory;
 	}
 }
